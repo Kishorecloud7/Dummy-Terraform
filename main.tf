2d8c4bafb041fe545e70a410546869bc24fcc750
Kishorecloud7 <kishore.be.org@gmail.com>
2025-11-24 12:40:27 +0530

    Modified

--- main.tf
+++ main.tf
@@ -1,7 +1,23 @@
-terraform { required_providers { docker = { source = "kreuzwerker/docker" version = "~> 3.0.2" } } }
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
+  }
+}
 
 provider "docker" {}
 
-resource "docker_image" "nginx_image" { name = "nginx:latest" keep_locally = true }
+resource "docker_image" "nginx_image" {
+  name = "nginx:latest"
+}
 
-resource "docker_container" "nginx_container" { name = "my-nginx" image = docker_image.nginx_image.name ports { internal = 80 external = 8080 } }
+resource "docker_container" "nginx_container" {
+  name  = "my-nginx"
+  image = docker_image.nginx_image.name
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
