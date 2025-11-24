0468b46ef037a5cdece09a52218f4414f304d99b
Kishorecloud7 <kishore.be.org@gmail.com>
2025-11-24 13:30:02 +0530

    Modified main file

--- main.tf
+++ main.tf
@@ -9,21 +9,17 @@ terraform {
 
 provider "docker" {}
 
-# Step 1: Read the latest image from Docker Hub
 resource "docker_registry_image" "nginx" {
   name = "nginx:latest"
 }
 
-# Step 2: Pull the image locally (CORRECT v3 syntax)
 resource "docker_image" "nginx_image" {
   pull {
     name = docker_registry_image.nginx.name
   }
-
   keep_locally = true
 }
 
-# Step 3: Create the container
 resource "docker_container" "nginx_container" {
   name  = "my-nginx"
   image = docker_image.nginx_image.image_id
