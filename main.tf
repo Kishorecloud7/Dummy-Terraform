1c3bce679ac8c8da5e745586a66e2a9b4cca61b4
Kishorecloud7 <kishore.be.org@gmail.com>
2025-11-24 13:01:24 +0530

    Modified main file

--- main.tf
+++ main.tf
@@ -9,13 +9,23 @@ terraform {
 
 provider "docker" {}
 
-resource "docker_image" "nginx_image" {
+# Get nginx from registry
+resource "docker_registry_image" "nginx" {
   name = "nginx:latest"
 }
 
+# Pull the image locally
+resource "docker_image" "nginx_image" {
+  name         = docker_registry_image.nginx.name
+  pull_triggers = [docker_registry_image.nginx.sha256_digest]
+  keep_locally = true
+}
+
+# Run the container
 resource "docker_container" "nginx_container" {
   name  = "my-nginx"
   image = docker_image.nginx_image.name
+
   ports {
     internal = 80
     external = 8080
