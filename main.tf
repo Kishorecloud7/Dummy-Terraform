a13483100805b8fc2a4334254b08a14d0bdb7d19
Kishorecloud7 <kishore.be.org@gmail.com>
2025-11-27 15:40:47 +0530

    Modified

--- main.tf
+++ main.tf
@@ -9,13 +9,16 @@ terraform {
 
 provider "docker" {}
 
+# Pull latest nginx image
 resource "docker_image" "nginx_image" {
   name = "nginx:latest"
+  keep_locally = true
 }
 
+# Create Container
 resource "docker_container" "nginx_container" {
   name  = "my-nginx"
-  image = docker_image.nginx_image.latest
+  image = docker_image.nginx_image.image_id
 
   ports {
     internal = 80
